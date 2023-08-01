99eb1b91c58eb1e743b5ac30971e9b8be2bde1d8
Sertac Ozercan <sozercan@gmail.com>
2023-08-01 07:17:27 +0000

    Brew formula update for kubectl-ai version v0.0.11

--- kubectl-ai.rb
+++ kubectl-ai.rb
@@ -5,20 +5,20 @@
 class KubectlAi < Formula
   desc "kubectl-ai is a kubectl plugin to generate and apply Kubernetes manifests using OpenAI GPT."
   homepage ""
-  version "0.0.10"
+  version "0.0.11"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.10/kubectl-ai_darwin_amd64.tar.gz"
-      sha256 "c3fe0604e356fd3bf96ec9d4f05e039ee40c8f83bf9f0fa22ee17c0379602612"
+    if Hardware::CPU.arm?
+      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.11/kubectl-ai_darwin_arm64.tar.gz"
+      sha256 "446b8f163bb61682bb6f44770a742637209a884c094a6a4f61294f6cd8c226d1"
 
       def install
         bin.install "kubectl-ai"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.10/kubectl-ai_darwin_arm64.tar.gz"
-      sha256 "6e739cb4e2ba41919c669bc96d1901ebb45397ae9e2e5e66f72bb069ca49a4e8"
+    if Hardware::CPU.intel?
+      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.11/kubectl-ai_darwin_amd64.tar.gz"
+      sha256 "2ee5ae161b2705aa6e382a08fa614238ad1b33f2b71be8482aac377cdecc7c8b"
 
       def install
         bin.install "kubectl-ai"
@@ -27,17 +27,17 @@ class KubectlAi < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.10/kubectl-ai_linux_amd64.tar.gz"
-      sha256 "bf1f5c73a983f11210584d97b22a3fc334c69706dfcb9a9ac6165f2ef875c345"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.11/kubectl-ai_linux_arm64.tar.gz"
+      sha256 "158eca02fab65556808724e99e4f5922bd0c34105c100950458fb9a7faf1107c"
 
       def install
         bin.install "kubectl-ai"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.10/kubectl-ai_linux_arm64.tar.gz"
-      sha256 "6c1e5297c7159e37eecb80ef40910402e7dcfacc1d10edef59a19958806515ca"
+    if Hardware::CPU.intel?
+      url "https://github.com/sozercan/kubectl-ai/releases/download/v0.0.11/kubectl-ai_linux_amd64.tar.gz"
+      sha256 "b991b085a59a47f6ccd0f4a907c8c082c449acadf2553938a7edf36522f6daf4"
 
       def install
         bin.install "kubectl-ai"
