b4e10afa8d2da8fa22b52f379ea732847a9ca639
Sertaç Özercan <852750+sozercan@users.noreply.github.com>
2024-03-08 15:50:23 -0800

    chore: remove deprecated apis (#105)
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -57,7 +57,7 @@ func RootCmd() *cobra.Command {
 		Long:         "kubectl-ai is a plugin for kubectl that allows you to interact with OpenAI GPT API.",
 		Version:      version,
 		SilenceUsage: true,
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		PersistentPreRun: func(_ *cobra.Command, _ []string) {
 			if *debug {
 				log.SetLevel(log.DebugLevel)
 				printDebugFlags()
@@ -110,7 +110,7 @@ func run(args []string) error {
 			s.Start()
 		}
 
-		completion, err = gptCompletion(ctx, oaiClients, args, *openAIDeploymentName)
+		completion, err = gptCompletion(ctx, oaiClients, args)
 		if err != nil {
 			return err
 		}
